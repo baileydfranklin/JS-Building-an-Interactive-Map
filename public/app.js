10864b7e34f73097f2b6d00f7860db59fbecd7e5
Bailey Franklin <baileydfranklin@gmail.com>
2023-02-09 21:41:44 -0700

    completed assignment

--- public/app.js
+++ public/app.js
@@ -1,32 +1,112 @@
 //Note: You do NOT need to create an external JS file for this project despite any instructions you may see in Canvas. This file, app.js, is the only external JS file you need.
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+    
+]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+
+var popup = L.popup()
+    .setLatLng([51.513, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
+
+    function onMapClick(e) {
+        alert("You clicked the map at " + e.latlng);
+    }
+    
+    map.on('click', onMapClick);
+
+    var popup = L.popup();
+
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("You clicked the map at " + e.latlng.toString())
+            .openOn(map);
+    }
+    
+    map.on('click', onMapClick);
 
 
 
-// Create map
 
 
 
 
-// Add openstreetmap tiles
-
-
-
-//Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
-
-
-// Create and add a geolocation marker
 
 
 
 
-// Draw the 2nd arrondissement
 
 
 
 
-// Metro station markers
+
+
+// // Create map:                                                       
+// const myMap = L.map('map', {
+//     center: [48.868672, 2.342130],
+//     zoom: 12,
+// });
 
 
 
 
-// Create red pin marker
+// // Add openstreetmap tiles
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//     minZoom: '15',
+// }).addTo(myMap)
+
+
+
+
+// //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
+
+
+// // Create and add a geolocation marker
+// const marker = L.marker([48.87007, 2.346453])
+// marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
+
+
+
+
+// // Draw the 2nd arrondissement                                        
+// [48.863368120198004, 2.3509079846928516]
+// [48.86933262048345, 2.3542531602919805]
+// [48.87199261164275, 2.3400569901592183]
+// [48.86993336274516, 2.3280142476578813] 
+// [48.86834104280146, 2.330308418109664]
+
+
+
+
+
+// // Metro station markers
+
+
+
+
+
+// // Create red pin marker
